18dd6f1ffadd994f0670da8caaacaa31300b3965
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-05 12:40:45 -0700

    Created and Tested GET FOLDER by ID - POSTMAN test passed

--- routes/folders.js
+++ routes/folders.js
@@ -13,4 +13,28 @@ router.get('/', (req, res, next) => {
     .catch(err => next(err));
 });
 
+router.get('/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  knex
+    .first('id', 'name')
+    .from('folders')
+    .modify(function (queryBuilder) {
+      if (id) {
+        queryBuilder.where('folders.id', `${id}`)
+      }
+    })
+    .then(folder => {
+      if (`!${id}`) {
+        res.json(folder);
+        res.status(200).send('OK');
+      } else {
+        res.status(404).send('NOT FOUND');
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
 module.exports = router;
