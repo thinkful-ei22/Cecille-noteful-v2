430919e5459c84af7edde6d8ba671d6c8f2cee94
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-05 12:58:49 -0700

    Created and Tested INSERT FOLDER - POSTMAN test passed

--- routes/folders.js
+++ routes/folders.js
@@ -68,4 +68,26 @@ router.put('/:id', (req, res, next) => {
     });
 })
 
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+
+  const newFolder = { name };
+
+  if (!newFolder.name) {
+      const err = new Error('Missing `name` in request body');
+      err.status = 400;
+      return next(err);
+  }
+
+  knex('folders')
+    .insert(newFolder)
+    .returning(['id', 'name'])
+    .then(folder => {
+      res.location(`http://${req.headers.host}/folders/${folder[0].id}`).status(201).send('CREATED FOLDER').json(folder[0]);
+    })
+    .catch((err => {
+      next(err);
+    }));
+});
+
 module.exports = router;
