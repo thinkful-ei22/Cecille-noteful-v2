98638329131d822db37f9befbeeefad1180cc396
Cecille Salazar <cecillersalazar@gmail.com>
2018-07-06 09:02:46 -0700

    Tested and tweaked the provided POST endpoint for tags.js - POSTMAN test passed

--- routes/tags.js
+++ routes/tags.js
@@ -4,5 +4,27 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../knex');
 
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+
+  /***** Never trust users. Validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newItem = { name };
+
+  knex ('tags')
+    .insert(newItem)
+    .returning(['id', 'name'])
+    .then((results) => {
+      // Uses Array index solution to get first item in results array
+      const result = results[0];
+      res.location(`http://${req.headers.host}/tags/${result.id}`).status(201).json(result);
+    })
+    .catch(err => next(err));
+});
 
 module.exports = router;
